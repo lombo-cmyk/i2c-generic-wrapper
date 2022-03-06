e5c28f9a31ae426e13618f68b360fc3bc9016e7b
lukaszk <srsrsrew@gmail.com>
2022-03-06 12:46:32 +0100

    Add template files

---
+++ include/I2CWrapper.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Lukasz on 06.03.2022.
+//
+
+#ifndef AIRCONTROLLER_I2CWRAPPER_HPP
+#define AIRCONTROLLER_I2CWRAPPER_HPP
+
+class I2CWrapper {};
+
+#endif // AIRCONTROLLER_I2CWRAPPER_HPP
---
+++ src/I2CWrapper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lukasz on 06.03.2022.
+//
+
+#include "../include/I2CWrapper.hpp"
